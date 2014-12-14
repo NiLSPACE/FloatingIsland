843f92bfae4c7d0e1bd9298ff16c243bd92ec7f8
STRWarrior <niels.breuker@hotmail.nl>
2014-12-14 11:49:10 +0100

    Increased the speed some more

--- generator.lua
+++ generator.lua
@@ -32,27 +32,30 @@ function cGeneratorFloatingIsland(a_Seed)
 				a_ChunkDesc:SetBiome(x, z, biPlains)
 				
 				local Noise0_1 = perlin_2d(xx / 25, zz / 25, m_Seed)
-				local Noise0_2 = perlin_2d(zz / 25, xx / 25, m_Seed)
 				
-				local Noise1 = math.min(Noise0_1, Noise0_2)
-				
-				if (Noise1 > 0) then
-					local Noise2 = math.max(Noise0_1, Noise0_2)
+				if (Noise0_1 > 0) then
+					local Noise0_2 = perlin_2d(zz / 25, xx / 25, m_Seed)
 					
-					local MaxHeight = math.ceil(64 + Noise1 * (10 * (Noise1 + 1) ^ 1.5))
-					local MinHeight = math.floor(55 - Noise2 * 25 + (((0.5 - Noise1) * 10) ^ 2))
+					local Noise1 = math.min(Noise0_1, Noise0_2)
 					
-					if (MaxHeight >= MinHeight) then
-						a_ChunkDesc:SetHeight(x, z, MaxHeight)
-					end
-					
-					MaxHeight = Clamp(MaxHeight, 0, 255)
-					MinHeight = Clamp(MinHeight, 0, 255)
-					
-					local Pattern = g_Patterns.patGrass
-					for I = MaxHeight, MinHeight, -1 do
-						local Block = Pattern[MaxHeight - I + 1] or E_BLOCK_STONE
-						a_ChunkDesc:SetBlockType(x, I, z, Block)
+					if (Noise1 > 0) then
+						local Noise2 = math.max(Noise0_1, Noise0_2)
+						
+						local MaxHeight = math.ceil(64 + Noise1 * (10 * (Noise1 + 1) ^ 1.5))
+						local MinHeight = math.floor(55 - Noise2 * 25 + (((0.5 - Noise1) * 10) ^ 2))
+						
+						if (MaxHeight >= MinHeight) then
+							a_ChunkDesc:SetHeight(x, z, MaxHeight)
+						end
+						
+						MaxHeight = Clamp(MaxHeight, 0, 255)
+						MinHeight = Clamp(MinHeight, 0, 255)
+						
+						local Pattern = g_Patterns.patGrass
+						for I = MaxHeight, MinHeight, -1 do
+							local Block = Pattern[MaxHeight - I + 1] or E_BLOCK_STONE
+							a_ChunkDesc:SetBlockType(x, I, z, Block)
+						end
 					end
 				end
 			end
